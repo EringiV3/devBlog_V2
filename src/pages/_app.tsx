bd48b8e63c6e56d24a66ac2ff1d0c6897802fe44
Kodai Hagihara <39426616+EringiV3@users.noreply.github.com>
2021-09-28 22:52:31 +0900

    add: GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,40 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import GoogleAnalytics from '../components/GoogleAnalytics';
 import { seoConfig } from '../config/seo';
 import '../global.css';
+import { existsGaId, pageview } from '../lib/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!existsGaId) {
+      return;
+    }
+
+    const handleRouteChange = (path: string) => {
+      pageview(path);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ChakraProvider>
-      <DefaultSeo {...seoConfig} />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider>
+        <DefaultSeo {...seoConfig} />
+        <GoogleAnalytics />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 export default MyApp;
